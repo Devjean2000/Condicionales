9ccd6fb756c4b4bc13603961d5e009b5f9cbab6c
Devjean2000 <112570027+Devjean2000@users.noreply.github.com>
2022-09-02 15:03:57 -0500

    Add files via upload
    En este archivo realizo el apartado del ejercicio: 
    
    "Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación."

---
+++ Ejercicioestaciones.java
@@ -0,0 +1,17 @@
+public class Ejercicioestaciones {
+  public static void main() {
+String estacion = "primavera"; 
+switch (estacion){
+ case "primavera":
+ System.out.println("Es primavera");
+case "verano": 
+System.out.println("Es verano"); 
+case "otoño":
+System.out.println("Es otoño");
+case "invierno":
+System.out.println("Es invierno");
+default:
+System.out.println("No es una estación");
+    }  
+  }   
+}  
