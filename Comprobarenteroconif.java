dabf4548a5af222f73e06874c108236bd55a43f2
Devjean2000 <112570027+Devjean2000@users.noreply.github.com>
2022-09-02 12:38:12 -0500

    Add files via upload
    En este primer archivo resuelvo el apartado del ejercicio:
    "Usando un if, crear una condición que compare si la variable numero es positivo, negativo, o 0."

---
+++ Comprobarenteroconif.java
@@ -0,0 +1,13 @@
+public class Comprobarenteroconif {
+    public static void main(String[] args) {
+	   int numero = 0; 
+		//Para este caso elegí el número 20 como variable
+		if (numero > 0) {
+			System.out.println("El número es positivo");
+		} else if (numero < 0) {
+			System.out.println("El número es negativo");
+		} else {
+			System.out.println("El número es neutro");
+		}
+	}
+}
